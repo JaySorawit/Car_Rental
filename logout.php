7abc59c726361afbc18c91e083ced1962afcb698
สรวิชญ์ โมกไธสง <jay@Macintosh.local>
2023-05-01 21:41:57 +0700

    fix logout

--- logout.php
+++ logout.php
@@ -10,6 +10,7 @@
         setcookie(session_name(), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
+            echo 'test';
         );
     }
     
