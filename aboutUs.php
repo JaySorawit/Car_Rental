b4ede21bd531ed7a57dc52f57b8011c7982d2af3
สรวิชญ์ โมกไธสง <jay@Macintosh.local>
2023-05-03 14:35:30 +0700

    add about us

--- aboutUs.php
+++ aboutUs.php
@@ -20,10 +20,42 @@
             include 'navbarclient.php';
         }
     ?>
-    <div class="container-sm">
-        <h1>About Us</h1>
-        <p>We are a company that specializes in...</p>
-    </div>
+        <div class="container-sm">
+            <div class="aboutus">
+                <h1> About Us</h1>
+                <p> <br>
+                    Welcome to our car rental website! We are a team of passionate car enthusiasts 
+                    who believe that everyone should have access to quality transportation at an 
+                    affordable price. Our goal is to make it easy for people to find and rent cars 
+                    from a trusted community of car owners.
+                </p>
+                <p> <br>
+                    Whether you're looking for a reliable car for your daily commute, a spacious SUV 
+                    for a family vacation, or a luxurious sports car for a special occasion, we have 
+                    a wide selection of vehicles to choose from. All of our cars are well-maintained 
+                    and regularly serviced to ensure your safety and comfort.
+                </p>
+                <p> <br>
+                    If you're a car owner looking to make some extra income, we invite you to join our 
+                    community and start renting out your car. We provide a simple and secure platform for
+                    you to list your car and connect with potential renters. You can choose your own rental 
+                    rates, availability, and we'll handle the rest, from marketing your car to processing 
+                    payments and providing insurance coverage.
+                </p>
+                <p><br>
+                    At our car rental website, we are committed to providing exceptional customer service 
+                    and support. If you have any questions or concerns, our friendly and knowledgeable team 
+                    is always here to help. We believe that renting a car should be a hassle-free and enjoyable 
+                    experience, and we strive to make that a reality for all of our customers.
+                </p>
+                <p> <br>
+                    Thank you for choosing our car rental website. We look forward to serving you and 
+                    helping you make the most of your travels.
+                </p>
+            </div>
+        </div> 
+    </main>
+
 
   <footer>
         <p> Copyright © 2023.</p>
