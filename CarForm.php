859a21a571c7a309868b06d317d86069fb5bf1c2
สรวิชญ์ โมกไธสง <jay@Macintosh.local>
2023-05-05 14:31:18 +0700

    fix register

---
+++ CarForm.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+    include 'server';
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rent out</title>
+    </head>
+    <body>
+        <?php include 'navbarclient.php' ?>
+        <main>
+            <h1> test</h1>
+        </main>
+        
+    </body>
+    </html>
--- Register.php
+++ Register.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-$con = mysqli_connect("localhost", "root", "", "CarRental_DB");
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$con = mysqli_connect("localhost", "root", "", "carrental_db");
+if (!$con) {
+    die('Could not connect: ' . mysqli_error());
 }
 
 
@@ -22,41 +21,54 @@ $my_date = $year . '-' . date('m', strtotime($month)) . '-' . str_pad($day, 2, '
 if (empty($email)) {
     $_SESSION['error'] = 'Please enter your email.';
     header("location: RegisterForm.php");
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+} 
+else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $_SESSION['error'] = 'Please enter a valid email.';
     header("location: RegisterForm.php");
-} else if (empty($password)) {
+} 
+else if (empty($password)) {
     $_SESSION['error'] = 'Please enter your password.';
     header("location: RegisterForm.php");
-} else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+} 
+else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
     $_SESSION['error'] = 'The password must be between 5 and 20 characters long.';
     header("location: RegisterForm.php");
-} else if (empty($c_password)) {
+} 
+else if (empty($c_password)) {
     $_SESSION['error'] = 'Please confirm your password.';
     header("location: RegisterForm.php");
-} else if (empty($firstname)) {
+} 
+else if (empty($firstname)) {
     $_SESSION['error'] = 'Please enter your first name.';
     header("location: RegisterForm.php");
-} else if (empty($lastname)) {
+} 
+else if (empty($lastname)) {
     $_SESSION['error'] = 'Please enter your last name.';
     header("location: RegisterForm.php");
-} else if (empty($tel_no)) {
+} 
+else if (empty($tel_no)) {
     $_SESSION['error'] = 'Please enter your mobile number.';
     header("location: RegisterForm.php");
-} else if (empty($gender)) {
+} 
+else if (empty($gender)) {
     $_SESSION['error'] = 'Please select your gender.';
     header("location: RegisterForm.php");
-} else if (!checkdate(date('m', strtotime($month)), $day, $year)) {
+} 
+else if (!checkdate(date('m', strtotime($month)), $day, $year)) {
     $_SESSION['error'] = 'Invalid date of birth.';
     header("location: RegisterForm.php");
-} else if (empty($_POST['interestedcar'])) {
+} 
+else if (empty($_POST['interestedcar'])) {
     $_SESSION['error'] = 'Please select the car you are interested in.';
     header("location: RegisterForm.php");
-} else if ($password != $c_password) {
+} 
+else if ($password != $c_password) {
     $_SESSION['error'] = 'Passwords do not match';
     header("location: RegisterForm.php");
-} else {
+} 
+else {
     try {
+       
         $check_email = "SELECT email FROM client WHERE email='$email'";
         $result = mysqli_query($con,$check_email);
         $row = mysqli_fetch_assoc($result);
@@ -64,13 +76,15 @@ if (empty($email)) {
         if ($row['email'] == $email) {
             $_SESSION['warning'] = "The email has already been taken.";
             header("location: RegisterForm.php");
-        } else if (!isset($_SESSION['error'])) {
+
+        } 
+        else if (!isset($_SESSION['error'])) 
+        {
             $sql1 = "INSERT INTO client (fname,lname,dateofbirth,gender,email,password,tel_no)
 	            VALUES ('$firstname', '$lastname', '$my_date','$gender','$email','$password','$tel_no')";
             if (!mysqli_query($con, $sql1)) {
                 die('Error: ' . mysqli_error($con));
             }
-
             $interested_cars = array_values($_POST['interestedcar']);
 
             for ($i = 0; $i < count($interested_cars); $i++) {
@@ -82,14 +96,16 @@ if (empty($email)) {
                 }
 
             }
-
+        
             $_SESSION['success'] = "You have Successfully Subscribed! <a href='LoginForm.php' class='alert-link'>Click Here!</a> to Login";
             header("location: RegisterForm.php");
-        } else {
+        } 
+        else {
             $_SESSION['error'] = "Something went wrong";
             header("location: RegisterForm.php");
         }
-    } catch (PDOException $e) {
+    } 
+    catch (PDOException $e) {
         echo $e->getMessage();
     }
 }
