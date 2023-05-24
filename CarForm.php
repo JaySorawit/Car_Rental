7846ec68d72125661d5f5a88aa84889ef2393657
สรวิชญ์ โมกไธสง <jay@Macintosh.ST-244F>
2023-05-25 00:21:13 +0700

    fix error div

--- CarForm.php
+++ CarForm.php
@@ -14,15 +14,22 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/carform.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <title>Rent out</title>
     </head>
     <body>
         <?php include 'navbarclient.php' ?>
-        <main>
+        <div class="container-md d-flex justify-content-center" style="height: auto;">
+        <div class="row bg-white">
+            <main>
             <h1> test</h1>
             <button id="edit-button" class="profile-submit" >Edit</button>
             <input type="submit" class="profile-submit" value="Submit">
-        </main>
+            </main>
+        </div>
+        </div>
+        
         
     </body>
     </html>
