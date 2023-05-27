7fd87325a43f653c6789e32ed5fe702982ae1ae1
worapholohm <133252333+worapholohm@users.noreply.github.com>
2023-05-27 13:46:52 +0700

    Ui index and Query viewcars

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-    session_start();
-    $loggedIn = isset($_SESSION['loggedIn']) ? $_SESSION['loggedIn'] : false;
+session_start();
+$loggedIn = isset($_SESSION['loggedIn']) ? $_SESSION['loggedIn'] : false;
 
-    include('server.php');
+include('server.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <meta charset="UTF-8">
@@ -13,199 +14,115 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Rental</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <style>
-        .search-wrapper {
-            position: relative;
-            width: 300px; 
-            margin: 0 auto; 
-            margin-bottom: 10px;
-        }
-
-        #suggestions-list1 {
-            position: absolute;
-            width: 100%;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            max-height: 200px;
-            overflow-y: auto;
-            z-index: 9999;
-            list-style: none; 
-            padding: 0; 
-            margin: 0; 
-        }
-
-        #suggestions-list1 li {
-            padding: 10px;
-            cursor: pointer;
-        }
-
-        #suggestions-list1 li:hover {
-            background-color: #e9e9e9;
-        }
-
-        #suggestions-list2 {
-            position: absolute;
-            width: 100%;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            max-height: 200px;
-            overflow-y: auto;
-            z-index: 9999;
-            list-style: none; 
-            padding: 0; 
-            margin: 0; 
-            
-        }
-
-        #suggestions-list2 li {
-            padding: 10px;
-            cursor: pointer;
-        }
-
-        #suggestions-list2 li:hover {
-            background-color: #e9e9e9;
-        }
-
-        #suggestions-list3 {
-            position: absolute;
-            width: 100%;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            max-height: 200px;
-            overflow-y: auto;
-            z-index: 9999;
-            list-style: none; 
-            padding: 0; 
-            margin: 0; 
-            
-        }
-        
-        #suggestions-list3 li {
-            padding: 10px;
-            cursor: pointer;
-        }
-
-        #suggestions-list3 li:hover {
-            background-color: #e9e9e9;
-        }
-
-        #search-input {
-            width: 100%;
-            padding: 10px;
-        }
-
-        .search-btn {
-            display: block;
-            width: 200px; 
-            margin: 0 auto;
-            padding: 10px;
-            background-color: #E48D0A;
-            color: #fff;
-            font-size: 16px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        .search-btn:hover {
-            background-color: #FFA41B;
-        }
-
-        .search-btn:active {
-            background-color: #000000; 
-        }
-
-    </style>
+    <script src="https://kit.fontawesome.com/b33e8d6cbf.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/litepicker/dist/litepicker.js"></script>
 </head>
+
 <body>
     <?php
-        if (!$loggedIn) {
-            include 'navbaruser.php';    
-        } else {
-            include 'navbarclient.php';
-        }
+    if (!$loggedIn) {
+        include 'navbaruser.php';
+    } else {
+        include 'navbarclient.php';
+    }
     ?>
-        <main>
-        <label for="start-date">Start Date:</label>
-            <input type="date" id="start-date" name="start-date">
-        <label for="end-date">End Date:</label>
-            <input type="date" id="end-date" name="end-date">
+    <div class="container-md d-flex justify-content-center align-items-center mt-3 text-white" style="height: 78vh;">
+        <main class="search-container">
+            <div class="mb-1">
+                <label for="start-date"><i class="fa-regular fa-calendar me-2"></i>Start Date &nbsp; </label>
+                <input type="text" id="start-date" name="start-date" class="date-info">
+                <span class="me-4"></span>
+                <label for="end-date"><i class="fa-regular fa-calendar me-2"></i>End Date &nbsp; </label>
+                <input type="text" id="end-date" name="end-date" class="date-info">
+            </div>
 
-        <br ><center>Province</center></br>
+            <br>
+            <center class="mb-2">Province</center>
 
-        <div class="search-wrapper">
-            <input type="text" id="province_selected" class="form-control" onkeyup="fetchSuggestions(this.value,'fetch_suggestions_province.php')">
-            <ul id="suggestions-list1"></ul>
-        </div>
+            <div class="search-wrapper">
+                <input type="text" id="province_selected" class="form-control"
+                    onkeyup="fetchSuggestions(this.value,'fetch_suggestions_province.php')" style="border: 1px solid #ccc;
+    background: #f9f9f9;">
+                <ul id="suggestions-list1" class ="text-black"></ul>
+            </div>
 
-        <br ><center>District</center></br>
+            <br>
+            <center class="mb-2">District</center>
 
-        <div class="search-wrapper">
-            <input type="text" id="district_selected" class="form-control" onkeyup="fetchSuggestions_district(this.value, 'fetch_suggestions_district.php', $('#province_selected').val())">
-            <ul id="suggestions-list2"></ul>
-        </div>
+            <div class="search-wrapper">
+                <input type="text" id="district_selected" class="form-control"
+                    onkeyup="fetchSuggestions_district(this.value, 'fetch_suggestions_district.php', $('#province_selected').val())" style="border: 1px solid #ccc;
+    background: #f9f9f9;">
+                <ul id="suggestions-list2" class ="text-black"></ul>
+            </div>
 
-        <br ><center>Brand</center></br>
+            <br>
+            <center class="mb-2">Brand</center>
 
-        <div class="search-wrapper">
-            <input type="text" id="brand_selected" class="form-control" onkeyup="fetchSuggestions(this.value,'fetch_suggestions_brand.php')">
-            <ul id="suggestions-list3"></ul>
-        </div>
-        
-        <div class="transmission-wrapper">
-        <label>
-            <input type="radio" name="transmission" value="auto"> Automatic
-        </label>
-        </div>
+            <div class="search-wrapper mb-3">
+                <input type="text" id="brand_selected" class="form-control"
+                    onkeyup="fetchSuggestions(this.value,'fetch_suggestions_brand.php')" style="border: 1px solid #ccc;
+    background: #f9f9f9;">
+                <ul id="suggestions-list3" class ="text-black"></ul>
+            </div>
 
-        <div class="transmission-wrapper">
-        <label>
-            <input type="radio" name="transmission" value="manual"> Manual Transmission
-        </label>
-        </div>
+            <div class="transmission-wrapper d-flex justify-content-center mb-4">
+                    <input class="me-1" type="radio" name="transmission" value="auto"> Automatic
+                    <span class="me-3"></span>
+                    <input class="me-1" type="radio" name="transmission" value="manual"> Manual Transmission
+            </div>
 
-        <button class="search-btn" onclick="search()">Find Car</button>
+            <button class="search-btn" onclick="search()">Find Car</button>
+            <script src="js/date.js"></script>
             <script>
-                function fetchSuggestions(keyword,url) {
-                    
+                function fetchSuggestions(keyword, url) {
+
                     $.ajax({
                         url: url,
                         method: 'POST',
-                        data: {keyword: keyword},
+                        data: { keyword: keyword },
                         success: function (data) {
                             var activeInput = $('.form-control:focus');
                             var suggestionsList;
 
                             if (activeInput.attr('id') === 'province_selected') {
                                 suggestionsList = $('#suggestions-list1');
-                            }  else if (activeInput.attr('id') === 'brand_selected') {
+                            } else if (activeInput.attr('id') === 'brand_selected') {
                                 suggestionsList = $('#suggestions-list3');
                             }
                             if (activeInput.attr('id') === 'province_selected') {
-                                 $('#district_selected').val('');
+                                $('#district_selected').val('');
                             }
                             suggestionsList.html(data);
                         }
                     });
                 }
                 function fetchSuggestions_district(keyword, url, province) {
-                $.ajax({
-                url: url,
-                method: 'POST',
-                data: {
-                    keyword: keyword,
-                    province: province
-                },
-                success: function (data) {
-                    var suggestionsList = $('#suggestions-list2');
-                    suggestionsList.html(data);
+                    $.ajax({
+                        url: url,
+                        method: 'POST',
+                        data: {
+                            keyword: keyword,
+                            province: province
+                        },
+                        success: function (data) {
+                            var suggestionsList = $('#suggestions-list2');
+                            suggestionsList.html(data);
+                        }
+                    });
                 }
-                });
-                }
-                function search() {
-    var startDate = document.getElementById('start-date').value;
-    var endDate = document.getElementById('end-date').value;
+    function formatDate(dateString) {
+    var date = new Date(dateString);
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1).toString().padStart(2, '0');
+    var day = date.getDate().toString().padStart(2, '0');
+    return year + '-' + month + '-' + day;
+}
+    function search() {
+    var startDate = formatDate(document.getElementById('start-date').value);
+    var endDate = formatDate(document.getElementById('end-date').value);
     var province = document.getElementById('province_selected').value;
     var district = document.getElementById('district_selected').value;
     var brand = document.getElementById('brand_selected').value;
@@ -217,37 +134,41 @@
     // Redirect to viewcars.php with the search parameters
     var params = "?start-date=" + startDate + "&end-date=" + endDate + "&province=" + province + "&district=" + district + "&brand=" + brand + "&transmission=" + transmission;
     window.location.href = "viewcars.php" + params;
+    console.log(startDate);
 }
 
-                
-                $(document).on('click', '#suggestions-list1 li', function() {
+                $(document).on('click', '#suggestions-list1 li', function () {
                     var selectedOption = $(this).text();
                     $('#province_selected').val(selectedOption);
                     $('#suggestions-list1').empty();
                 });
 
-                $(document).on('click', '#suggestions-list2 li', function() {
+                $(document).on('click', '#suggestions-list2 li', function () {
                     var selectedOption = $(this).text();
                     $('#district_selected').val(selectedOption);
                     $('#suggestions-list2').empty();
                 });
-                $(document).on('click', '#suggestions-list3 li', function() {
+                $(document).on('click', '#suggestions-list3 li', function () {
                     var selectedOption = $(this).text();
                     $('#brand_selected').val(selectedOption);
                     $('#suggestions-list3').empty();
                 });
-                $(document).on('click', function(e) {
-        
-                if (!$(e.target).closest('.search-wrapper').length) {
-                    $('#suggestions-list1').empty();
-                    $('#suggestions-list2').empty();
-                    $('#suggestions-list3').empty();
-                }
+                $(document).on('click', function (e) {
+
+                    if (!$(e.target).closest('.search-wrapper').length) {
+                        $('#suggestions-list1').empty();
+                        $('#suggestions-list2').empty();
+                        $('#suggestions-list3').empty();
+                    }
                 });
             </script>
+            
         </main>
+    </div>
+
     <footer>
         <p> Copyright © 2023.</p>
     </footer>
 </body>
+
 </html>
--- viewcars.php
+++ viewcars.php
@@ -16,32 +16,38 @@
       FROM address
       WHERE province = '$province'
       AND district = '$district'
-    ) AND model_id IN (
+    )
+    AND model_id IN (
       SELECT model_id
       FROM brand_info
       WHERE brand = '$brand'
-    ) 
-    AND license_plate IN (SELECT license_plate
-    FROM rent_info
-    WHERE '$startDate' NOT BETWEEN start_date AND end_date
-      AND '$endDate' NOT BETWEEN start_date AND end_date
-    )AND transmission ='$transmission';
+    )
+    AND transmission = '$transmission'
+    AND (
+      license_plate NOT IN (
+        SELECT license_plate
+        FROM rent_info
+        WHERE ('$startDate' BETWEEN start_date AND end_date)
+          OR ('$endDate' BETWEEN start_date AND end_date)
+      )
+      OR NOT EXISTS (
+        SELECT license_plate
+        FROM rent_info
+        WHERE car_info.license_plate = rent_info.license_plate
+      ));
     "; 
 
     $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
 
-    // ตรวจสอบการเชื่อมต่อฐานข้อมูล
+
     if (!$conn) {
         die("Error: " . mysqli_connect_error());
     }
 
-    // ส่งคำสั่ง SQL ไปยังฐานข้อมูล
+
     $result = mysqli_query($conn, $query);
 
-    // ตรวจสอบผลลัพธ์ของคำสั่ง SQL
-    
 
-    // ปิดการเชื่อมต่อฐานข้อมูล
     mysqli_close($conn);
 ?>
 <!DOCTYPE html>
@@ -55,9 +61,51 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
+       
+        h2 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        p {
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
+
+        .car-item {
+            background-color: #f9f9f9;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
         
+        .car-item img {
+            width: 100%;
+            height: auto;
+            margin-bottom: 10px;
+        }
+
+        .car-details {
+            text-align: center;
+        }
+
+        .car-details a {
+            display: inline-block;
+            padding: 8px 16px;
+            background-color: #E48D0A;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
+            margin-top: 10px;
+        }
+
+        .car-details a:hover {
+            background-color: #FFA41B;
+        }
     </style>
 </head>
+
 <body>
     <?php
         if (!$loggedIn) {
@@ -66,20 +114,26 @@
             include 'navbarclient.php';
         }
     ?>
+     <div class="location">
+        <!-- <p>Location: <?php echo $province; ?>, <?php echo $district; ?></p> -->
+        <!-- <p>Date: <?php echo $startDate; ?> to <?php echo $endDate; ?></p>
+        <p>brand: <?php echo $brand; ?> to <?php echo $transmission; ?></p> -->
+    </div>
        <main>
     <div class="car-list">
         <?php
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '<div class="car-item">';
-                //echo '<img src="' . $row['car_image'] . '" alt="Car Image">'; // เพิ่มบรรทัดนี้เพื่อแสดงรูปภาพรถ
+                //echo '<img src="' . $row['car_image'] . '" alt="Car Image">'; // แสดงรูปภาพรถ
                 echo '<div class="car-details">';
                 echo '<h2>' . $brand . ' ' . $row['model_id'] . '</h2>';
-                echo '<p>License Plate: ' . $row['license_plate'] . '</p>';
+                //echo '<p>License Plate: ' . $row['license_plate'] . '</p>';
                 echo '<p>Transmission: ' . $row['transmission'] . '</p>';
                 echo '<p>Color: ' . $row['color'] . '</p>';
                 echo '<p>Seat: ' . $row['seat'] . '</p>';
+                echo '<p>Years: ' . $row['year_car'] . '</p>';
                 echo '<p>Price: ' . $row['price_per_day'] . ' per day</p>';
-                echo '<a href="RentalForm.php?car_id=' . $row['license_plate'] . '">Book Now</a>'; // เพิ่มลิงก์ "Book Now" และส่งพารามิเตอร์ car_id
+                echo '<a href="RentalForm.php?car_id=' . $row['license_plate'] . '">Book Now</a>'; 
                 echo '</div>';
                 echo '</div>';
             }
