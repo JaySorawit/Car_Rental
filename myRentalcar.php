99162a41abcd31143e35b46ced029494fe86a48e
worapholohm <133252333+worapholohm@users.noreply.github.com>
2023-05-27 20:40:21 +0700

    Rent History

--- myRentalcar.php
+++ myRentalcar.php
@@ -7,7 +7,8 @@ $query = "SELECT * FROM rent_info
 JOIN car_info ON rent_info.license_plate = car_info.license_plate 
 JOIN brand_info ON car_info.model_id = brand_info.model_id 
 JOIN address ON car_info.district = address.district AND car_info.zipcode = address.zipcode
-WHERE rent_info.client_id = (select client_id from client where email='$email');";
+WHERE rent_info.client_id = (SELECT client_id FROM client WHERE email='$email')
+ORDER BY rent_info.start_date;";
 
 $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
 
@@ -37,8 +38,49 @@ $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/b33e8d6cbf.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/litepicker/dist/litepicker.js"></script>
+    <link rel="stylesheet" href="css/carform.css">
     <style>
+    h2 {
+        font-size: 24px;
+        margin-bottom: 10px;
+    }
 
+    p {
+        font-size: 16px;
+        margin-bottom: 5px;
+    }
+
+    .car-item {
+        background-color: #f9f9f9;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    .car-item img {
+        width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+    }
+
+    .car-details {
+        text-align: center;
+    }
+
+    .car-details a {
+        display: inline-block;
+        padding: 8px 16px;
+        background-color: #E48D0A;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 4px;
+        margin-top: 10px;
+    }
+
+    .car-details a:hover {
+        background-color: #FFA41B;
+    }
 
     </style>
 </head>
@@ -54,13 +96,13 @@ $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
     <div class="container-md d-flex justify-content-center" style="height: auto; ">
         <div class="row bg-white">
                 <div class="carshow">
-                    <div class="titlecar" style="margin-bottom: 30px;">My car</div>
+                    <div class="titlecar" style="margin-bottom: 30px;">Rental History</div>
                     <div class="listcar">
                         <?php while ($row = mysqli_fetch_assoc($result)) { ?>
                         <div class="boxofcar">
-                            <!-- <div class="imgofcar">
-                                <img src="<?php echo $row['image_path']; ?>">
-                            </div> -->
+                        <div class="imgofcar">
+                                <!-- <img src="<?php echo $row['image_path']; ?>"> -->
+                            </div>
                             <div class="info_car">
                                 <div class="car_info">
                                     <div class="carinfotitle">
@@ -73,9 +115,9 @@ $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
                                             <p>Brand: <?php echo $row['brand']; ?></p>
                                             <p>Model: <?php echo $row['model_id']; ?></p>
                                             <p>Year: <?php echo $row['year_car']; ?></p>
+                                            <p>Transmission: <?php echo $row['transmission']; ?></p>
                                         </div>
                                         <div class="boxinfocar">
-                                            <p>Transmission: <?php echo $row['transmission']; ?></p>
                                             <p>Color: <?php echo $row['color']; ?></p>
                                             <p>Seat: <?php echo $row['seat']; ?></p>
                                             <p>Price per Day: <?php echo $row['price_per_day']; ?></p>
@@ -87,12 +129,20 @@ $conn = mysqli_connect('localhost', 'root', '', 'carrental_db');
                                 <div class="car_address">
                                     <div class="carinfotitle">
                                         <img src="">
-                                        <h4>Rent Address</h4>
+                                        <h4>Rented Address</h4>
                                     </div>
                                     <div class="boxinfocar">
                                         <p>Province: <?php echo $row['province']; ?></p>
                                         <p>District: <?php echo $row['district']; ?></p>
-                                        
+                                        <?php
+                                            $start_date = strtotime($row['start_date']);
+                                            $end_date = strtotime($row['end_date']);
+                                            $num_days = ($end_date - $start_date) / (60 * 60 * 24);
+                                            $total_cost = $row['price_per_day'] * $num_days;
+                                            $formatted_total_cost = number_format($total_cost, 2);
+                                            ?>
+                                            <p>Total Cost: </p>
+                                            <p style="font-size: 25px; color: red;"><?php echo $formatted_total_cost; ?> THB</p>
 
                                     </div>
                                 </div>
