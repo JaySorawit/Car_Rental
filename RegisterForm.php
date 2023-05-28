038fb46102a9ef1f7a72a360307216670e4a8574
Jay Sorawit <ganglovenok@gmail.com>
2023-05-28 11:21:10 +0700

    check form

--- RegisterForm.php
+++ RegisterForm.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
-$con = mysqli_connect("localhost", "root", "", "carrental_db");
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+include "server.php"
 ?>
 
 <!DOCTYPE html>
